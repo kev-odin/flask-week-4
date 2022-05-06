687ce054790da7d7e61fbd3ed2711825db7a6036
Kev <kchung93@uw.edu>
2022-05-05 21:20:11 -0700

    bugfix: user added to db

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo, Email
+from wtforms import StringField, PasswordField, SubmitField, DateField, IntegerField
+from wtforms.validators import DataRequired, Length, Email, NumberRange
 from flask_login import current_user, login_user, login_required, logout_user
 from models import db, login, UserModel
 from wiki import find_birthdays
 
 
-class loginForm(FlaskForm):
+class LoginForm(FlaskForm):
     email = StringField(label="Enter email", validators=[DataRequired(), Email()])
     password = PasswordField(
         label="Enter password", validators=[DataRequired(), Length(min=6, max=16)]
@@ -15,8 +15,22 @@ class loginForm(FlaskForm):
     submit = SubmitField(label="Login")
 
 
-# passwords={}
-# passwords['lhhung@uw.edu']='qwerty'
+class RegisterForm(FlaskForm):
+    email = StringField(label="Enter email", validators=[DataRequired(), Email()])
+    password = PasswordField(
+        label="Enter password", validators=[DataRequired(), Length(min=6, max=16)]
+    )
+    submit = SubmitField(label="Register")
+
+
+class SearchBirthday(FlaskForm):
+    birthday = DateField(label="Enter birthday", validators=[DataRequired()])
+    num_results = IntegerField(
+        label="Number of results",
+        validators=[DataRequired(), NumberRange(min=1, max=20)],
+    )
+    submit = SubmitField(label="Search")
+
 
 app = Flask(__name__)
 app.secret_key = "a secret"
@@ -27,7 +41,7 @@ db.init_app(app)
 login.init_app(app)
 
 
-def addUser(email, password):
+def add_user(email, password):
     # check if email or username exits
     user = UserModel()
     user.set_password(password)
@@ -41,13 +55,25 @@ def create_table():
     db.create_all()
     user = UserModel.query.filter_by(email="lhhung@uw.edu").first()
     if user is None:
-        addUser("lhhung@uw.edu", "qwerty")
+        add_user("lhhung@uw.edu", "qwerty")
 
 
-@app.route("/home")
+@app.route("/home", methods=["GET", "POST"])
 @login_required
-def find_birthdays():
-    return render_template("home.html", myData=find_birthdays())
+def search_dates():
+    form = SearchBirthday()
+    if form.validate_on_submit():
+        if request.method == "POST":
+            entry = request.form["birthday"]
+            results = request.form["results"]
+            month = f"{entry[5:7]} / {entry[8:10]}"
+            year = entry[0:4]
+            return render_template(
+                "home.html", form=form, myData=find_birthdays(month, year, results)
+            )
+    return render_template(
+        "home.html", form=form, myData=find_birthdays("06/02", "1993", 10)
+    )
 
 
 @app.route("/")
@@ -57,7 +83,7 @@ def redirectToLogin():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    form = loginForm()
+    form = LoginForm()
     if form.validate_on_submit():
         if request.method == "POST":
             email = request.form["email"]
@@ -69,10 +95,37 @@ def login():
     return render_template("login.html", form=form)
 
 
+@app.route("/registration", methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if request.method == "POST":
+            email = request.form["email"]
+            password = request.form["password"]
+            user = UserModel.query.filter_by(email=email).first()
+            if user is None:
+                add_user(email, password)
+                flash("Registration Completed!")
+                return redirect("/login")
+            elif user is not None and user.check_password(password):
+                flash(f"Welcome {user}!")
+                login_user(user)
+                return redirect("/home")
+            else:
+                flash("WRONG!")
+    return render_template("registration.html", form=form)
+
+
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect("/login")
+
+
+@app.route("/forbidden", methods=["GET", "POST"])
+@login_required
+def protected():
+    return redirect(url_for("forbidden.html"))
 
 
 if __name__ == "__main__":
